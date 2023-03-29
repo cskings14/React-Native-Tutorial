0cf950837ff4a55f333ed5539006d551ad9ddb49
cskings14 <cskings14@gmail.com>
2023-03-29 10:42:40 -0400

    added layout and index

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+// View functions as a div tag. Text functions as a p or h1 tag.
+
+
+const Home = () => {
+    return (
+        <View>
+        <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
