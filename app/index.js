79c19412cf916cf08ec4c6289612f3e4f0732833
cskings14 <cskings14@gmail.com>
2023-04-12 05:16:44 -0400

    added more to index

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+    // View serves as render for react-native
+    // Text serves as a h1 tag for react-native
+    // Button only works as a closing tag
   return (
-    <View style={styles.container}>
+    <View style={styles.container}> 
       <Text>Hello World!!</Text>
       <Text>I am testing the server</Text>
+      <Button title="ee oo" />
       <StatusBar style="auto" />
     </View>
   );
